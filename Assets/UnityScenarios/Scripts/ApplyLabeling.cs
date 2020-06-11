e2010d58b9f67f2e0b4b4478a31fe87401aa1886
Priyesh Wani <priyesh@unity3d.com>
2020-06-10 21:50:40 -0700

    Converting the project to URP

---
+++ Assets/UnityScenarios/Scripts/ApplyLabeling.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Perception.GroundTruth;
+
+public class ApplyLabeling : MonoBehaviour
+{
+    public GameObjectTags        m_Tags;
+    public Camera                m_Camera;
+    public LabelingConfiguration m_LabelingConfig;
+    
+    void Awake()
+    {
+        int id = 1, startValue = 10000;
+        foreach (var tag in m_Tags.tags)
+        {
+            var gameobjects = GameObject.FindGameObjectsWithTag(tag);
+            foreach (var go in gameobjects)
+            {
+                var labelingComponent = go.AddComponent<Labeling>();
+                labelingComponent.labels = new List<string>() { tag };
+            }
+            m_LabelingConfig.LabelEntries.Add(new LabelEntry(id++, tag, startValue++));
+        }
+
+        var perceptionCamera = m_Camera.GetComponent<PerceptionCamera>();
+        if (perceptionCamera == null)
+            m_Camera.gameObject.AddComponent<PerceptionCamera>();
+
+        perceptionCamera.LabelingConfiguration = m_LabelingConfig;
+        perceptionCamera.produceSegmentationImages = true;
+    }
+}
